a2a61aca1d7be54a6aad9699e2ef703d9df810e1
humyPro <1106805538@qq.com>
2018-08-04 10:21:59 +0800

    修改了文件上传js

--- login/src/main/java/com/gossip/login/controller/Person.java
+++ login/src/main/java/com/gossip/login/controller/Person.java
@@ -1,7 +1,16 @@
 package com.gossip.login.controller;
 
+import com.gossip.login.controller.service.UserService;
+import com.gossip.login.vo.SysResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author humy
@@ -12,5 +21,14 @@ public class Person {
     @GetMapping("person")
     public String person(){
         return "person";
+    }
+
+    @Autowired
+    private UserService userService;
+    @PostMapping("/user/fileupload/{ticket}")
+    public String fileUpload(@PathVariable String ticket, MultipartFile picture, HttpServletRequest request, HttpServletResponse response){
+
+        String image=userService.changeHeadPic(ticket,picture,request,response);
+        return image;
     }
 }
--- login/src/main/resources/static/js/login.js
+++ login/src/main/resources/static/js/login.js
@@ -15,7 +15,10 @@ $(function () {
         return login();
     });
     //登录成功后页面变化
-
+    var ticket = getCookie("USER_TICKET");
+    if (ticket.length != 0) {
+        changeMeau(ticket);
+    }
 
 });
 function login() {
@@ -91,3 +94,28 @@ function login() {
 //         return true;
 //     }
 // }
+
+function changeMeau(ticket) {
+    $("#meau_bar").html(
+        "<ul class=\"layui-nav pull-left\">" +
+        "<li class=\"layui-nav-item layui-this\"><a href=\"index\">首页</a></li>" +
+        "<li class=\"layui-nav-item\"><a href=\"message.html\">留言</a></li>" +
+        "<li class=\"layui-nav-item\"><a href=\"about.html\">关于</a></li>" +
+        "<li class=\"layui-nav-item\">" +
+        "</li>" +
+        "</ul>" +
+        /*图片需要换乘user.headpic*/
+        "<a href=\"/user/" + ticket + "\">" +
+        "<img class=\"last_mouse\" src=\"/head/12114ca4ba7dde9d0400c54d23a1d5de.jpg\">" +
+        "</a>"
+    );
+    //鼠标悬停时样式变化
+    $(".last_mouse").mouseover(function () {
+        $(this).addClass("get_mouse");
+    });
+    //鼠标离开时样式变化
+    $(".last_mouse").mouseout(function () {
+        $(this).removeClass("get_mouse");
+    });
+    //鼠标点击时进入首页
+}
